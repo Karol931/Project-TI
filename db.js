16a08ae5a0b34b0dee2f328f0d66aa354d38200d
Karol931 <66036778+Karol931@users.noreply.github.com>
2023-01-24 22:57:03 +0100

    first commit

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
-const dotenv = require("dotenv")
-dotenv.config()
-const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
 const db = mongoose.connection;
 
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.CONNECTIONSTRING, { useUnifiedTopology: true, useUnifiedTopology: true }, function (err, client) {
-    module.exports = client
-    const app = require("./server.js")
-    app.listen(process.env.PORT || 20727)
-})
+mongoose.set("strictQuery", false);
+mongoose.connect(
+  process.env.CONNECTIONSTRING,
+  { useUnifiedTopology: true, useUnifiedTopology: true },
+  function (err, client) {
+    module.exports = client;
+    const app = require("./server.js");
+    app.listen(process.env.PORT || 20727, "0.0.0.0");
+  }
+);
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"))
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
